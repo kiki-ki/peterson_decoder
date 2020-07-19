8598336f02810288063bd4ef0702b5c78ae8c4d3
nakata kai <malpie0619@gmail.com>
2020-07-20 08:12:22 +0900

    ビットごとの排他的論理和を計算

--- peterson_decrypt.rb
+++ peterson_decrypt.rb
@@ -1,26 +1,37 @@
-def syndrome(m, y, e)
-  s = []
-  map = { "0" => 0, "1" => 1 }
+def syndrome(m, t, y, e)
+  # ビットマップ作成
+  map = { "0" => 0, "z0" => 1 } # z0: 1 (便宜上、"z0"としている)
   (1..(2**m - 2)).each do |i|
-    map["z#{i}"] = i <= m - 1 ?
-      2**i : i == m ? map["z#{i - (m - 1)}"] ^ map["1"] : map["z#{i - (m - 1)}"] ^ map["z#{i - m}"]
+    map["z#{i}"] = i <= m - 1 ? 2**i : map["z#{i - (m - 1)}"] ^ map["z#{i - m}"]
   end
-  map
+
+  # シンドローム計算
+  s = (2 * t).times.map do
+    total = map["z#{y[0]}"]
+
+    # ビットごとの排他的論理和を計算
+    (1..(y.size - 1)).each do |i|
+      total_arr = total.to_s(2).rjust(m, "0").split("")
+      map_arr = map["z#{y[i]}"].to_s(2).rjust(m, "0").split("")
+
+      total = total_arr.each_with_index.map do |v, idx|
+        (total_arr[idx].to_i ^ map_arr[idx].to_i).to_s
+      end.join.to_i(2)
+    end
+
+    total
+  end
+  p s
 end
 
-puts syndrome(5, 1, 1)
 
 # 有限体GF(2^m)
-# m = 4
-# n = 2^m - 1           # 符号長
-# dist = 5              # 設計距離: 2個以下の誤りを訂正できる最小値
-# kiyaku = 1 + x + x^4  # 規約多項式
+m = 4
+dist = 5                  # 設計距離: 2個以下の誤りを訂正できる最小値
+t = ((dist - 1) / 2).to_i # 訂正可能な誤りの個数
+n = 2**m - 1              # 符号長
+kiyaku = [0, 1, 4]        # 規約多項式: 1 + x + x**4
+e = [7, 8]                # 誤り箇所(2箇所): x**7 + x**8
+y = [0, 4, 6]             # 受信語: 1 + x**4 + x**6
 
-# e = x^7 + x^8         # 誤り箇所(2箇所)
-# y = 1 + x^4 + x^6     # 受信語
-
-
-# s =
-# z_map {
-#   0: 0b0000, 1: 0b0001,
-# }
+puts syndrome(m, t, y, e)
